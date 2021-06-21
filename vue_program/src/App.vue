48f08a9131df63035dbcbd7c7bc87b158a331200
hcq0313 <853801007@qq.com>
2021-06-21 16:18:11 +0800

    vue前端tab切换

--- vue_program/src/App.vue
+++ vue_program/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <Test></Test> -->
+    <tabZujian></tabZujian>
+
     <router-view/>
   </div>
 </template>
 
 <script>
+import Test from './components/test.vue'
+import tabZujian from './components/tabZujian.vue'
+// import './assets/icons/iconfont'
 export default {
+  components:{
+    // Test,
+    tabZujian,
+
+
+  },
   name: 'App'
 }
 </script>
